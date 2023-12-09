38f76ec219c08e33497f3cb0340bb28184c1f08e
usmanrangrez <usmaanrangrez@gmail.com>
2023-12-10 01:58:41 +0530

    updated git ignore

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
 
 //config env
 dotenv.config();
@@ -22,17 +21,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
-//for deploy
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 //PORT
 const PORT = process.env.PORT || 8080;
